05363bb31947b7dff9c62f97c66fb8381b5dc54a
zablon <zablonoigo51@gmail.com>
2024-11-28 06:00:25 +0300

    fix the missing curly bracket

--- index.js
+++ index.js
@@ -53,17 +53,16 @@ const createPost=async (event)=>{
     }
     return response;
 }
-const updatePost= async(event)=>{
-    const response={statusCode:200};
+const updatePost = async (event) => {
+    const response = { statusCode: 200 };
 
-    try{
-        const body=JSON.parse(event.body);
-        const objKeys=Object.keys(body);
-        const parama={
-            TableName:process.env.DYNAMODB_TABLE_NAME,
+    try {
+        const body = JSON.parse(event.body);
+        const objKeys = Object.keys(body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
             Key: marshall({ postId: event.pathParameters.postId }),
-            UpdateExpression:
-             `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+            UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
             ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
                 ...acc,
                 [`#key${index}`]: key,
@@ -73,26 +72,26 @@ const updatePost= async(event)=>{
                 [`:value${index}`]: body[key],
             }), {})),
         };
+
         const updateResult = await db.send(new UpdateItemCommand(params));
 
         response.body = JSON.stringify({
             message: "Successfully updated post.",
             updateResult,
         });
-
-        }catch (e) {
-            console.error(e);
-            response.statusCode = 500;
-            response.body = JSON.stringify({
-                message: "Failed to update post.",
-                errorMsg: e.message,
-                errorStack: e.stack,
-            });
-        }
-    
-        return response;
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to update post.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
     }
-}
+
+    return response;
+};
+
 const deletePost=async (event)=>{
     const response={statusCode:200}
 }
